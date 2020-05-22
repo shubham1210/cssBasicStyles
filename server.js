007f44df16532d911eb776a4574232f7c1bc4d98
Shubham Sharma <shubham1210@users.noreply.github.com>
2020-05-22 22:40:54 +0530

    First Commit : basic concepts of css : positioning , basic css properties.
    https://www.youtube.com/watch?v=yfoY53QXEnI

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+let path = require('path');
+
+app = express();
+
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+app.use(express.static(path.join(__dirname + '/html-css/')));
+
+
+
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
